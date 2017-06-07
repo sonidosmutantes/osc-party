ebc05697302e1525a8c5b81172ae3759021f24c8
Hernán Ordiales <hordiales@gmail.com>
2017-06-07 02:24:08 -0300

    JAM OSC

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>MARDER OSC MONITOR</title>
+<title>JAM OSC MONITOR</title>
 <link href="styles/style.css" rel="stylesheet" type="text/css" />
 
 <?php 
