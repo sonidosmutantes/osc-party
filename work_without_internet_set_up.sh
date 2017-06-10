7ae240bc4b7e0b82f8596cb3bc4483814781df15
Hernán Ordiales <hordiales@gmail.com>
2017-06-10 03:36:22 -0300

    hack to work without an iternet connection

--- work_without_internet_set_up.sh
+++ work_without_internet_set_up.sh
@@ -2,8 +2,8 @@ cd src
 
 wget http://code.jquery.com/jquery-1.8.2.min.js
 
-wget https://www.google.com/uds/api/visualization/1.0/84dc8f392c72d48b78b72f8a2e79c1a1/format+ru,default+ru,u
-i+ru,table+ru,corechart+ru.I.js -O visualization.js
+wget "https://www.google.com/uds/api/visualization/1.0/84dc8f392c72d48b78b72f8a2e79c1a1/format+ru,default+ru,ui+ru,table+ru,corechart+ru.I.js" -O visualization.js
 
-mv ni_graph.php graph.php
+cp graph.php graph.php.orginal
+cp ni_graph.php graph.php
 
